bd2c818ab8f04cfbe2ea9303010f5eb4ec2fba15
Matt Lantin <mlantin1093@gmail.com>
2024-06-09 23:33:06 -0400

    Implemented Firebase into 4 sections of Home Page & Started Work on Detail Page

--- disney-clone/src/components/Home.js
+++ disney-clone/src/components/Home.js
@@ -1,12 +1,67 @@
 import styled from 'styled-components';
 import ImgSlider from './ImgSlider';
 import Viewers from './Viewers';
+import Recommended from './Recommend';
+import NewDisney from './NewDisney';
+import Originals from './Originals';
+import Trending from './Trending';
+
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { db } from "../firebase"; 
+import { setMovies } from '../features/movie/movieSlice';
+import { selectUserName } from "../features/user/userSlice";
+import { collection, onSnapshot } from "firebase/firestore";
 
 const Home = (props) => {
+
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+
+  let recommends = [];
+  let newDisneys = [];
+  let originals = [];
+  let trending = [];
+
+  useEffect(() => {
+    const unsub = onSnapshot(collection(db, "movies"), (snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        switch (doc.data().type) {
+          case "recommend":
+            recommends.push({ id: doc.id, ...doc.data() });
+            break;
+          case "new":
+            newDisneys.push({ id: doc.id, ...doc.data() });
+            break;
+          case "original":
+            originals.push({ id: doc.id, ...doc.data() });
+            break;
+          case "trending":
+            trending.push({ id: doc.id, ...doc.data() });
+            break;
+          default:
+            break;
+        }
+      });
+      dispatch(setMovies({
+        recommend: recommends,
+        newDisney: newDisneys,
+        original: originals,
+        trending: trending
+      }));
+    });
+
+    return () => unsub(); // Cleanup subscription on unmount
+  }, [userName]);
+      
     return (
         <Container>
             <ImgSlider/>
             <Viewers/>
+            <Recommended/>
+            <NewDisney/>
+            <Originals/>
+            <Trending/>
         </Container>
     );
 };
