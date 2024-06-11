444c9f1d293cb7c949dc585f8382920e4ed5df21
Matt Lantin <mlantin1093@gmail.com>
2024-06-11 08:15:36 -0400

    Finished Detail Paged and Refactored Header

--- disney-clone/src/components/Header.js
+++ disney-clone/src/components/Header.js
@@ -33,7 +33,6 @@ const Header = (props) => {
       signInWithPopup(auth, provider)
       .then((result) => {
           setUser(result.user);
-
       })
       .catch((error) => {
           alert(error.message);
@@ -210,7 +209,8 @@ const Login = styled.a`
   border: 1px solid #f9f9f9;
   border-radius: 4px;
   transition: all 0.2s ease 0s;
-
+  cursor: pointer;
+  
   &:hover {
     background-color: #f9f9f9;
     color: #000;
