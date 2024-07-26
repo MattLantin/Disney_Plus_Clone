aee01bd94b346a179047cf1db0da843ddca08304
Matthew Lantin <127584120+MattLantin@users.noreply.github.com>
2024-07-26 17:16:02 -0400

    Update firebase.js .env

--- disney-clone/src/firebase.js
+++ disney-clone/src/firebase.js
@@ -5,13 +5,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCcBBErSWyxpkoqxA77xIoBf2n_aPhjRV0",
-  authDomain: "disneyplus-clone-3d8c3.firebaseapp.com",
-  projectId: "disneyplus-clone-3d8c3",
-  storageBucket: "disneyplus-clone-3d8c3.appspot.com",
-  messagingSenderId: "944066809385",
-  appId: "1:944066809385:web:da38172db0a0ef76905638",
-  measurementId: "G-3H9L2L4QYC"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
